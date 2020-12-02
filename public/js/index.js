f32d313b42bcf521f2dd5f331b3203105ebe3535
Gabi <gari.327@gmail.com>
2020-12-02 11:06:23 +0100

    push final

--- public/js/index.js
+++ public/js/index.js
@@ -2,30 +2,55 @@
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var url = 'https://restcountries.eu/rest/v2/all';
+/* const url = `https://restcountries.eu/rest/v2/all`
 
-var getCountry = function () {
-    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(countryCode) {
-        var result, countries, country;
+
+
+const getCountry = async (countryCode) => {
+    const result = await axios.get(url)
+    const countries = result.data
+    const country = countries.find((country)=>{
+        return country.alpha2Code.toLowerCase() === countryCode.toLowerCase()
+    })
+    console.log(country.name);
+    render (country.name)
+    return country
+    
+}
+
+const country = getCountry('ES') */
+
+var getCountryByIp = function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+        var result1, countryCode, result2, countries, country;
         return regeneratorRuntime.wrap(function _callee$(_context) {
             while (1) {
                 switch (_context.prev = _context.next) {
                     case 0:
                         _context.next = 2;
-                        return axios.get(url);
+                        return axios.get('https://ipinfo.io/json?token=83974b22092886');
 
                     case 2:
-                        result = _context.sent;
-                        countries = result.data;
+                        result1 = _context.sent;
+                        countryCode = result1.data.country;
+
+                        console.log('ipinfo.io nos da este resultado', result1.data.country);
+                        ///podemos consultar todos los paises a restcountry
+                        _context.next = 7;
+                        return axios.get('https://restcountries.eu/rest/v2/all');
+
+                    case 7:
+                        result2 = _context.sent;
+                        countries = result2.data;
+
+                        console.log('restcountry nos da este resultado:', countries[240].alpha2Code);
                         country = countries.find(function (country) {
-                            return country.alpha2Code.toLowerCase() === countryCode.toLowerCase();
+                            return country.alpha2Code === countryCode;
                         });
 
-                        console.log(country.name);
-                        render('Spain');
-                        return _context.abrupt('return', country);
+                        render(country.name);
 
-                    case 8:
+                    case 12:
                     case 'end':
                         return _context.stop();
                 }
@@ -33,16 +58,15 @@ var getCountry = function () {
         }, _callee, undefined);
     }));
 
-    return function getCountry(_x) {
+    return function getCountryByIp() {
         return _ref.apply(this, arguments);
     };
 }();
-
-var country = getCountry('ES');
+getCountryByIp();
 
 var appRoot = document.getElementById('appRoot');
 
-var render = function render(name) {
+var render = function render(country) {
     var template = React.createElement(
         'div',
         null,
@@ -50,7 +74,7 @@ var render = function render(name) {
             'h1',
             null,
             'Este es el pais: ',
-            name,
+            country,
             ' '
         )
     );
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-const url = `https://restcountries.eu/rest/v2/all`
+/* const url = `https://restcountries.eu/rest/v2/all`
+
 
 
 const getCountry = async (countryCode) => {
@@ -8,24 +9,41 @@ const getCountry = async (countryCode) => {
         return country.alpha2Code.toLowerCase() === countryCode.toLowerCase()
     })
     console.log(country.name);
-    render ('Spain')
+    render (country.name)
     return country
     
 }
 
-const country = getCountry('ES')
+const country = getCountry('ES') */
 
+
+const getCountryByIp = async () => {
+    const result1 = await axios.get('https://ipinfo.io/json?token=83974b22092886')
+
+    const countryCode = result1.data.country
+    console.log('ipinfo.io nos da este resultado', result1.data.country);
+    ///podemos consultar todos los paises a restcountry
+    const result2 = await axios.get('https://restcountries.eu/rest/v2/all')
+    const countries = result2.data
+    console.log('restcountry nos da este resultado:', countries[240].alpha2Code);
+    const country = countries.find((country) => {
+        return country.alpha2Code === countryCode
+    })
+    render(country.name)
+}
+getCountryByIp()
 
 
 const appRoot = document.getElementById('appRoot')
 
 
-const render = (name)=>{
+const render = (country) => {
     const template = (
         <div>
-        <h1>Este es el pais: {name} </h1>
-           </div>
+            <h1>Este es el pais: {country} </h1>
+        </div>
+
     )
     ReactDOM.render(template, appRoot)
-    }
+}
 
